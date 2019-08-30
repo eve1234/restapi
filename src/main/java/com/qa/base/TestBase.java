803c11bcc2c0e5356c6fe0c773b9eb2f08a44919
eve1234 <gorgeous65@msn.com>
2019-08-30 07:05:45 +0100

    first commit

---
+++ src/main/java/com/qa/base/TestBase.java
@@ -0,0 +1,38 @@
+package com.qa.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+	
+	 //DO not hardcode status assertion results condition 
+    public int RESPONSE_STATUS_CODE_200 =200;
+    public int RESPONSE_STATUS_CODE_500 =500;
+    public int RESPONSE_STATUS_CODE_400 =400;
+    public int RESPONSE_STATUS_CODE_401 =401;
+    public int RESPONSE_STATUS_CODE_201 =201;
+    
+	public Properties prop;
+	
+	public TestBase(){
+		//read property file
+		 try{
+	            prop=new Properties();
+	            FileInputStream f= new FileInputStream(System.getProperty("user.dir")+"/src/main/java"
+	                    + "/com/qa/config/config.properties");
+	            prop.load(f);
+	            
+	            
+	        
+	        }catch(FileNotFoundException e){
+	            e.printStackTrace();
+	            
+	        }catch(IOException e){
+	            e.printStackTrace();
+	        }
+		
+	}
+
+}
---
+++ src/test/java/com/qa/test/PutAPITest.java
@@ -0,0 +1,15 @@
+package com.qa.test;
+
+import com.qa.base.TestBase;
+import com.qa.client.RestClient;
+
+public class PutAPITest {
+	TestBase testBase;
+	String serviceUrl;
+	String apiUrl;
+	String url;
+	RestClient restClient;
+	
+	
+
+}
